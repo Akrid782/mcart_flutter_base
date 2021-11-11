3bce9581318b4d86f11ea30fdc14b38cabcefbfe
Akrid <Akrid782@mail.ru>
2021-11-11 21:23:22 +0300

    google lab two

--- lib/page/code_lab.dart
+++ lib/page/code_lab.dart
@@ -10,13 +10,50 @@ class Laba extends StatefulWidget {
 
 class _LabaState extends State<Laba> {
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (context) {
+        final tiles = _saved.map((suggestion) {
+          return ListTile(
+            title: Text(
+              suggestion.asCamelCase,
+              style: _biggerFont,
+            ),
+          );
+        });
+
+        final divided = tiles.isNotEmpty
+            ? ListTile.divideTiles(
+                context: context,
+                tiles: tiles,
+              ).toList()
+            : <Widget>[];
+
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Сохраненные предложения'),
+          ),
+          body: ListView(children: divided),
+        );
+      }),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('CodeLab'),
+        actions: [
+          IconButton(
+            onPressed: _pushSaved,
+            icon: const Icon(Icons.list),
+            tooltip: 'Сохраненные предложения',
+          )
+        ],
       ),
       body: _buildSuggestions(),
     );
@@ -41,11 +78,26 @@ class _LabaState extends State<Laba> {
   }
 
   Widget _buildRow(WordPair suggestion) {
+    final alreadySaved = _saved.contains(suggestion);
     return ListTile(
       title: Text(
         suggestion.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+        semanticLabel: alreadySaved ? 'Удалить из сохраненных' : 'Save',
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(suggestion);
+          } else {
+            _saved.add(suggestion);
+          }
+        });
+      },
     );
   }
 }
