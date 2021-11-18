955e99dd62a7f73261442ff3eea8f1a8e3e8219d
Akrid <Akrid782@mail.ru>
2021-11-18 09:35:26 +0300

    Асинхронное взаимодействие

---
+++ lib/models/photos.dart
@@ -0,0 +1,25 @@
+class Photos {
+  int albumId;
+  int id;
+  String title;
+  String url;
+  String thumbnailUrl;
+
+  Photos({
+    required this.albumId,
+    required this.id,
+    required this.title,
+    required this.url,
+    required this.thumbnailUrl,
+  });
+
+  factory Photos.fromJson(Map<String, dynamic> json) {
+    return Photos(
+      albumId: json['albumId'],
+      id: json['id'],
+      title: json['title'],
+      url: json['url'],
+      thumbnailUrl: json['thumbnailUrl'],
+    );
+  }
+}
